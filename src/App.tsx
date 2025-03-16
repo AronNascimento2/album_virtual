2a7b6bed044106d967667ae6b0ed6e927be3c87b
AronNascimento2 <aronviniciusnascimento@gmail.com>
2025-03-16 18:43:32 -0300

    feat: create

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./home";
+import { Navbar } from "./NavBar";
+import { weddingPhotos } from "./weddingPhotos";
+import { SectionPage } from "./sectionPage";
+import { MusicProvider } from "./MusicContext";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <MusicProvider>
+      <Router>
+        <Navbar />
+        <div className="pt-16">
+          <Routes>
+            <Route path="/" element={<Home weddingPhotos={weddingPhotos} />} />
+            {Object.entries(weddingPhotos).map(([section, photos]) => (
+              <Route
+                key={section}
+                path={`/${section}`}
+                element={<SectionPage section={section} photos={photos} />}
+              />
+            ))}
+          </Routes>
+        </div>
+      </Router>
+    </MusicProvider>
+  );
+};
 
-export default App
+export default App;
