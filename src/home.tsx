69446cbc04ae4ecb19b6e693cee21ee60b594fd6
AronNascimento2 <aronviniciusnascimento@gmail.com>
2025-03-16 20:03:42 -0300

    fix: photo home

--- src/home.tsx
+++ src/home.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useState, useMemo, useCallback } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import { Navigation } from "swiper/modules";
@@ -7,11 +7,10 @@ import { useMusic } from "./MusicContext";
 import { X } from "lucide-react";
 import { TitleProps } from "./weddingPhotos";
 
-const chunkArray = <T,>(array: T[], size: number): T[][] => {
-  return Array.from({ length: Math.ceil(array.length / size) }, (_, index) =>
+const chunkArray = <T,>(array: T[], size: number): T[][] =>
+  Array.from({ length: Math.ceil(array.length / size) }, (_, index) =>
     array.slice(index * size, index * size + size)
   );
-};
 
 interface WeddingPhotos {
   cerimonia: string[];
@@ -28,75 +27,67 @@ interface HomeProps {
 const Home: React.FC<HomeProps> = ({ weddingPhotos }) => {
   const [fullScreenImage, setFullScreenImage] = useState<string | null>(null);
   const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
-  const [loadingImages, setLoadingImages] = useState<Record<string, boolean>>(
-    {}
-  );
-
-  const handleImageLoad = (index: string) => {
-    setLoadingImages((prev) => ({ ...prev, [index]: false }));
-  };
 
   const { handleUserInteraction } = useMusic();
-  const allImages = Object.values(weddingPhotos).flat();
+  const allImages = useMemo(
+    () => Object.values(weddingPhotos).flat(),
+    [weddingPhotos]
+  );
 
-  const handleNextImage = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    setCurrentImageIndex((prev) => (prev + 1) % allImages.length);
-    setFullScreenImage(allImages[(currentImageIndex + 1) % allImages.length]);
-  };
+  const handleNextImage = useCallback(
+    (e: React.MouseEvent) => {
+      e.stopPropagation();
+      setCurrentImageIndex((prev) => (prev + 1) % allImages.length);
+      setFullScreenImage(allImages[(currentImageIndex + 1) % allImages.length]);
+    },
+    [allImages, currentImageIndex]
+  );
 
-  const handlePrevImage = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    setCurrentImageIndex(
-      (prev) => (prev - 1 + allImages.length) % allImages.length
-    );
-    setFullScreenImage(
-      allImages[(currentImageIndex - 1 + allImages.length) % allImages.length]
-    );
-  };
+  const handlePrevImage = useCallback(
+    (e: React.MouseEvent) => {
+      e.stopPropagation();
+      setCurrentImageIndex(
+        (prev) => (prev - 1 + allImages.length) % allImages.length
+      );
+      setFullScreenImage(
+        allImages[(currentImageIndex - 1 + allImages.length) % allImages.length]
+      );
+    },
+    [allImages, currentImageIndex]
+  );
 
   return (
     <div
       className="container mx-auto p-4 max-w-7xl relative"
       onClick={handleUserInteraction}
     >
-      {/* Fundo fixo */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 0.5 }}
-        transition={{ duration: 1 }}
-        className="fixed inset-0 bg-cover bg-center z-[-1]"
+      {/* Fundo fixo otimizado */}
+      <div
+        className="fixed inset-0 bg-cover bg-center z-[-1] opacity-50"
         style={{
           backgroundImage: "url('casamento1/CERIMÔNIA OFICIAL/HF_-23.jpg')",
           backgroundAttachment: "fixed",
         }}
-      ></motion.div>
+      ></div>
 
-      {/* Título */}
-      <motion.h1
-        className="text-[100px] font-bold text-center mb-8 text-black"
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
-      >
+      {/* Título com menos animação */}
+      <h1 className="text-[100px] font-bold text-center mb-8 text-black">
         Aron e Índira
-      </motion.h1>
+      </h1>
 
       {/* Seções de Fotos */}
       {Object.entries(weddingPhotos).map(([section, photos]) => {
         const groupedPhotos = chunkArray(photos, 6);
         return (
-          <motion.div
+          <div
             key={section}
             id={section}
             className="mb-12 p-6 rounded-xl shadow-lg bg-white bg-opacity-80"
-            initial={{ opacity: 0.5, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8 }}
           >
             <h2 className="text-3xl font-semibold capitalize mb-6 text-center">
               {section.replace(/([A-Z])/g, " $1")}
             </h2>
+
             <Swiper
               navigation
               modules={[Navigation]}
@@ -119,17 +110,11 @@ const Home: React.FC<HomeProps> = ({ weddingPhotos }) => {
                           setCurrentImageIndex(allImages.indexOf(photo));
                         }}
                       >
-                        {loadingImages[`${index}-${i}`] && (
-                          <div className="absolute inset-0 flex items-center justify-center bg-gray-200 animate-pulse">
-                            Carregando...
-                          </div>
-                        )}
                         <img
-                          src={String(photo)}
+                          src={photo}
                           alt={`${section} ${i + 1}`}
                           className="w-full h-full object-cover rounded-xl"
-                          onLoad={() => handleImageLoad(`${index}-${i}`)}
-                          onError={() => handleImageLoad(`${index}-${i}`)}
+                          loading="lazy"
                         />
                       </motion.div>
                     ))}
@@ -137,11 +122,11 @@ const Home: React.FC<HomeProps> = ({ weddingPhotos }) => {
                 </SwiperSlide>
               ))}
             </Swiper>
-          </motion.div>
+          </div>
         );
       })}
 
-      {/* Imagem em Tela Cheia */}
+      {/* Imagem em Tela Cheia otimizada */}
       {fullScreenImage && (
         <motion.div
           className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-80 flex items-center justify-center z-50"
@@ -152,19 +137,19 @@ const Home: React.FC<HomeProps> = ({ weddingPhotos }) => {
           onClick={() => setFullScreenImage(null)}
         >
           <button
-            className="absolute top-5 right-5 text-black text-4xl font-bold hover:cursor-pointer"
+            className="absolute top-5 right-5 text-black text-4xl font-bold"
             onClick={() => setFullScreenImage(null)}
           >
             <X size={30} />
           </button>
           <button
-            className="absolute left-5 text-black text-6xl hover:cursor-pointer"
+            className="absolute left-5 text-black text-6xl"
             onClick={handlePrevImage}
           >
             &#10094;
           </button>
           <motion.img
-            src={fullScreenImage ?? ""}
+            src={fullScreenImage}
             alt="Fullscreen"
             className="max-w-full max-h-full"
             initial={{ scale: 0.8 }}
@@ -172,9 +157,8 @@ const Home: React.FC<HomeProps> = ({ weddingPhotos }) => {
             exit={{ scale: 0.8 }}
             transition={{ duration: 0.5 }}
           />
-
           <button
-            className="absolute right-5 text-black text-6xl hover:cursor-pointer"
+            className="absolute right-5 text-black text-6xl"
             onClick={handleNextImage}
           >
             &#10095;
